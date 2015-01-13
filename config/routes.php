db1d569aab721879fd9df73c5abab07e0c924254
Robert Scherer <ich@robertscherer.de>
2015-01-13 17:15:30 +0100

    adding FrontendBridge jsonAction default routes

--- config/routes.php
+++ config/routes.php
@@ -2,5 +2,7 @@
 use Cake\Routing\Router;
 
 Router::plugin('FrontendBridge', function ($routes) {
+	Router::connect('/json_action/:plugin/:controller/:action/*', array('jsonAction' => true));
+	Router::connect('/json_action/:controller/:action/*', array('jsonAction' => true));
 	$routes->fallbacks();
 });
