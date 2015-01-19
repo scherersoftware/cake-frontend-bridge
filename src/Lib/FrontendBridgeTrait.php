b17fc55702f999bd48af5e6638b2a4a6965b7747
Felix Kempf <felix.kempf@codekanzlei.de>
2015-01-19 19:18:54 +0100

    adjust to coding standard

--- src/Lib/FrontendBridgeTrait.php
+++ src/Lib/FrontendBridgeTrait.php
@@ -2,26 +2,40 @@
 namespace FrontendBridge\Lib;
 
 trait FrontendBridgeTrait {
-    protected function jsonActionResponse(\Cake\Network\Response $response) {
-        // get the frontendData set by the Frontend plugin and remove unnecessary data
-        $frontendData = $this->viewVars['frontendData'];
-        unset($frontendData['Types']);
-        $response = array(
-            'code' => 'success',
-            'data' => array(
-                'frontendData' => $frontendData,
-                'html' => $response->body()
-            )
-        );
-        return new \FrontendBridge\Lib\ServiceResponse($response);
-    }
 
-    public function renderJsonAction($view, $layout) {
-        if($layout === null) {
-            $layout = 'FrontendBridge.json_action';
-        }
-        $this->getView()->subDir = null;
-        $response = parent::render($view, $layout);
-        return $this->jsonActionResponse($response);
-    }
+/**
+ * jsonActionResponse
+ *
+ * @param \Cake\Network\Response $response the response
+ * @return \FrontendBridge\Lib\ServiceResponse
+ */
+	protected function jsonActionResponse(\Cake\Network\Response $response) {
+		// get the frontendData set by the Frontend plugin and remove unnecessary data
+		$frontendData = $this->viewVars['frontendData'];
+		unset($frontendData['Types']);
+		$response = array(
+			'code' => 'success',
+			'data' => array(
+				'frontendData' => $frontendData,
+				'html' => $response->body()
+			)
+		);
+		return new \FrontendBridge\Lib\ServiceResponse($response);
+	}
+
+/**
+ * renderJsonAction
+ *
+ * @param string $view   the view to render
+ * @param string $layout the layout to render
+ * @return \FrontendBridge\Lib\ServiceResponse
+ */
+	public function renderJsonAction($view, $layout) {
+		if ($layout === null) {
+			$layout = 'FrontendBridge.json_action';
+		}
+		$this->getView()->subDir = null;
+		$response = parent::render($view, $layout);
+		return $this->jsonActionResponse($response);
+	}
 }
