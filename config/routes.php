06c5f313dbcb1cb2f3fb0a88733b86a6f4afd138
Robert Scherer <ich@robertscherer.de>
2015-02-08 13:47:57 +0100

    Switch to DashedRoute for jsonActions

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,15 @@
 use Cake\Routing\Router;
 
 Router::plugin('FrontendBridge', function ($routes) {
-	Router::connect('/json_action/:plugin/:controller/:action/*', array('jsonAction' => true));
-	Router::connect('/json_action/:controller/:action/*', array('jsonAction' => true));
-	$routes->fallbacks('InflectedRoute');
+	Router::connect('/json_action/:plugin/:controller/:action/*', [
+		'jsonAction' => true
+	], [
+		'routeClass' => 'DashedRoute'
+	]);
+	Router::connect('/json_action/:controller/:action/*', [
+		'jsonAction' => true
+	], [
+		'routeClass' => 'DashedRoute'
+	]);
+	$routes->fallbacks('DashedRoute');
 });
