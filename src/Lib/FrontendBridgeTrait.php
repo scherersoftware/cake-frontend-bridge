641a151261bf12af9fde217f76f0658fa8208906
Felix Kempf <fk@scherer-software.de>
2015-03-13 10:19:52 +0100

    fix order of Controller::render and view instantiation in renderJsonAction case

--- src/Lib/FrontendBridgeTrait.php
+++ src/Lib/FrontendBridgeTrait.php
@@ -9,19 +9,19 @@ trait FrontendBridgeTrait {
  * @param \Cake\Network\Response $response the response
  * @return \FrontendBridge\Lib\ServiceResponse
  */
-	protected function jsonActionResponse(\Cake\Network\Response $response) {
-		// get the frontendData set by the Frontend plugin and remove unnecessary data
-		$frontendData = $this->viewVars['frontendData'];
-		unset($frontendData['Types']);
-		$response = array(
-			'code' => 'success',
-			'data' => array(
-				'frontendData' => $frontendData,
-				'html' => $response->body()
-			)
-		);
-		return new \FrontendBridge\Lib\ServiceResponse($response);
-	}
+    protected function jsonActionResponse(\Cake\Network\Response $response) {
+        // get the frontendData set by the Frontend plugin and remove unnecessary data
+        $frontendData = $this->viewVars['frontendData'];
+        unset($frontendData['Types']);
+        $response = array(
+            'code' => 'success',
+            'data' => array(
+                'frontendData' => $frontendData,
+                'html' => $response->body()
+            )
+        );
+        return new \FrontendBridge\Lib\ServiceResponse($response);
+    }
 
 /**
  * renderJsonAction
@@ -30,23 +30,23 @@ trait FrontendBridgeTrait {
  * @param string $layout the layout to render
  * @return \FrontendBridge\Lib\ServiceResponse
  */
-	public function renderJsonAction($view, $layout) {
-		if ($layout === null) {
-			$layout = 'FrontendBridge.json_action';
-		}
-		$this->getView()->subDir = null;
-		$response = parent::render($view, $layout);
-		return $this->jsonActionResponse($response);
-	}
+    public function renderJsonAction($view, $layout) {
+        if ($layout === null) {
+            $layout = 'FrontendBridge.json_action';
+        }
+        $response = parent::render($view, $layout);
+        $this->getView()->subDir = null;
+        return $this->jsonActionResponse($response);
+    }
 
 /**
  * Detect if the current request should be rendered as a JSON Action
  *
  * @return bool
  */
-	protected function _isJsonActionRequest() {
-		return 
-			(isset($this->request->params['jsonAction']) && $this->request->params['jsonAction'] === true)
-			|| $this->request->query('json_action') == 1;
-	}
+    protected function _isJsonActionRequest() {
+        return
+            (isset($this->request->params['jsonAction']) && $this->request->params['jsonAction'] === true)
+            || $this->request->query('json_action') == 1;
+    }
 }
