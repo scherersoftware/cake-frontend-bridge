49c50ae6601d0163aaa666c78b8843d82cd2bd6d
Jonathan Neugber <jn@scherer-software.de>
2017-06-28 11:27:54 +0200

    Simplify bootstrap file

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,23 +1,8 @@
 <?php
-use Cake\Http\ServerRequest;
+Cake\Network\Request::addDetector('dialog', function($request) {
+    return $request->query('dialog_action') === '1' || $request->param('dialogAction') === true;
+});
 
-(function() {
-    $isDialog = null;
-    $isJson = null;
-
-    ServerRequest::addDetector('dialog', function(ServerRequest $request) use ($isDialog) {
-        if (is_null($isDialog)) {
-            $isDialog = $request->query('dialog_action') === '1' || $request->param('dialogAction') === true;
-        }
-
-        return $isDialog;
-    });
-
-    ServerRequest::addDetector('json', function(ServerRequest $request) use ($isJson) {
-        if (is_null($isJson)) {
-            $isJson = $request->query('json_action') === '1' || $request->param('jsonAction') === true;
-        }
-
-        return $isJson;
-    });
-})();
+Cake\Network\Request::addDetector('json', function($request) {
+    return $request->query('json_action') === '1' || $request->param('jsonAction') === true;
+});
